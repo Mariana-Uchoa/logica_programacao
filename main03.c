e44c8c85af09e7e0d0c2b477d354008f4510e1c4
Mariana-Uchoa <uchoamariana1530@gmail.com>
2025-08-06 16:42:10 -0300

    Add files via upload

---
+++ main03.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    float y;
+    float s;
+    
+    x=30;
+    y=5.5;
+    s=x/y;
+    
+    printf("A divisão de x e y eh: %f", s);
+
+    return 0;
+}
